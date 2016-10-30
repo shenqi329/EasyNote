dd568537b8643521fb68840d93037ae00d092fba
shenqi329 <shenqi329@163.com>
2016-10-30 13:55:10 +0800

    xx
    xx

--- controller/loginController.go
+++ controller/loginController.go
@@ -1,17 +1,26 @@
 package controller
 
 import (
-	"fmt"
-	"easynote/bean"
-	"easynote/service"
+	//"fmt"
+	//"easynote/bean"
+	//"easynote/service"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
-func GetRegister(c echo.Context) error {
-	fmt.Println("GetRegister");
-	service.IsUserExist()
-	test := bean.User{Name: "liujunshi", Password: "123456"}
-	return c.JSON(http.StatusOK, test)
+var (
+	users = []string{"Joxxe", "Veerxx", "Zioxxn"}
+)
 
+func GetRegister(c echo.Context) error {
+	return c.JSON(http.StatusOK, users)
 }
+
+
+// func GetRegister(c echo.Context) error {
+// 	fmt.Println("GetRegister");
+// 	service.IsUserExist()
+// 	test := bean.User{Name: "liujunshi", Password: "123456"}
+// 	return c.JSON(http.StatusOK, test)
+
+// }
