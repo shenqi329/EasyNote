d899617f6fd1e298e4fb1a23bc4e147e6a38669a
liujunshi <524607630@qq.com>
2016-11-14 20:18:56 +0800

    x

--- controller/loginController.go
+++ controller/loginController.go
@@ -3,13 +3,14 @@ package controller
 import (
 	"easynote/bean"
 	"easynote/service"
-	"github.com/labstack/echo"
-	"net/http"
 	"encoding/json"
-	_ "os"
 	"fmt"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"net/http"
+	_ "os"
+	"time"
 )
-
 
 func IsUserExist(c echo.Context) error {
 
@@ -17,29 +18,40 @@ func IsUserExist(c echo.Context) error {
 
 	isUserExist := service.IsUserExist(userName)
 
-	userExistBean := bean.UserExistBean{Exist:isUserExist,Name:userName}
+	userExistBean := bean.UserExistBean{Exist: isUserExist, Name: userName}
 
-	data,_ := json.Marshal(userExistBean);
+	data, _ := json.Marshal(userExistBean)
 
-	responseBean := bean.ResponseBean{Code:"000001",Desc:"success",Data:string(data)}
-	
+	responseBean := bean.ResponseBean{Code: "000001", Desc: "success", Data: string(data)}
+
+	cookie := &standard.Cookie{&http.Cookie{
+		Name:  "session",
+		Value: "securetoken",
+		//Path:    "/",
+		Domain:  "192.168.99.100",
+		Expires: time.Date(2016, time.January, 1, 0, 0, 0, 0, time.UTC),
+		//Expires: time.Date(2016, time.December, 1, 0, 0, 0, 0, time.UTC),
+		//Secure:  true,
+		//HttpOnly: true,
+	}}
+	c.SetCookie(cookie)
 	return c.JSON(http.StatusOK, responseBean)
 
 }
 
-func UserRegister(c echo.Context) error{
+func UserRegister(c echo.Context) error {
 
 	user := new(bean.UserBean)
-	if error := c.Bind(user) ; error != nil {
+	if error := c.Bind(user); error != nil {
 		return error
 	}
-	fmt.Println("username:"+user.Name+"password"+user.Password)
+	fmt.Println("username:" + user.Name + "password" + user.Password)
 
-	if(!service.UserRegister(user)){
-		responseBean := bean.ResponseBean{Code:"000002",Desc:"resource is exist"}
-		return c.JSON(http.StatusOK,responseBean);
+	if !service.UserRegister(user) {
+		responseBean := bean.ResponseBean{Code: "000002", Desc: "resource is exist"}
+		return c.JSON(http.StatusOK, responseBean)
 	}
-	
-	responseBean := bean.ResponseBean{Code:"000001",Desc:"success"}
-	return c.JSON(http.StatusOK,responseBean);
+
+	responseBean := bean.ResponseBean{Code: "000001", Desc: "success"}
+	return c.JSON(http.StatusOK, responseBean)
 }
--- deploy_develop.sh
+++ deploy_develop.sh
@@ -6,3 +6,7 @@ docker run -it -p 80:80 -v /go/src/easynote:/go/src/easynote -v /go/src/easynote
 else
 docker start -i easynote
 fi
+
+
+
+#docker run -it -p 80:80 -p 27017:27017 -p 8081:8081 -p 8082:8082 -p 8888:22 -v /go/src/easynote:/go/src/easynote -v /go/src/easynote_data:/go/src/easynote_data --name easynote shenqi329/easynote /bin/bash
